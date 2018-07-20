3f6b2125adbe28055f4801f4c519707c86fa852c
Aleksandar Milanov <coatkog@gmail.com>
2018-07-20 23:23:03 +0200

    (added MERN stack template and user authentification)

---
+++ react/frontend/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+  }
+
+  componentDidMount() {
+    axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwtToken');
+    axios.get('/api/book')
+      .then(res => {
+        this.setState({ books: res.data });
+        console.log(this.state.books);
+      })
+      .catch((error) => {
+        if(error.response.status === 401) {
+          this.props.history.push("/login");
+        }
+      });
+  }
+
+  logout = () => {
+    localStorage.removeItem('jwtToken');
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <div class="container">
+        {localStorage.getItem('jwtToken') &&
+          <button class="btn btn-primary" onClick={this.logout}>Logout</button>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
