91eb903cb920731daacf605054bfe5e83bd3fec7
Aleksandar Milanov <coatkog@gmail.com>
2018-08-20 20:09:58 +0200

    (sockets) added remove node, edge and graph change

--- backend/reactive-model/graph.js
+++ backend/reactive-model/graph.js
@@ -11,12 +11,24 @@ module.exports = function(io) {
       graph.emit(rcv.username, {graph: rcv.graph});
     });
 
+    client.on('graph change', (rcv) => {
+      graph.emit('graph change', {graph: rcv.graph});
+    });
+
     client.on('add node', (rcv) => {
       graph.emit(rcv.room + ' add node', {sender: rcv.sender, node: rcv.node});
     });
 
+    client.on('remove node', (rcv) => {
+      graph.emit(rcv.room + ' remove node', {sender: rcv.sender, node: rcv.node});
+    });
+
     client.on('add edge', (rcv) => {
       graph.emit(rcv.room + ' add edge', {sender: rcv.sender, edge: rcv.edge});
+    });
+
+    client.on('remove edge', (rcv) => {
+      graph.emit(rcv.room + ' remove edge', {sender: rcv.sender, edge: rcv.edge});
     });
 
     client.on('compete begin', (rcv) => {
