aae01f9fad5534e4f1a4df545a2f11fa5c8a5b0f
Dusan Jovanovic <dusandjovanovic@outlook.com>
2019-08-20 22:19:08 +0200

    [refactor] modal display

--- src/containers/home/room-new/roomNew.js
+++ src/containers/home/room-new/roomNew.js
@@ -2,6 +2,7 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Grow from "@material-ui/core/Grow";
 import PropTypes from "prop-types";
 
 import { styles } from "./stylesheet";
@@ -15,28 +16,36 @@ const RoomNew = props => (
 		className={props.classes.root}
 	>
 		<Grid item xs={12}>
-			<Typography variant="h4" color="primary" gutterBottom>
-				Hi {props.username}
-			</Typography>
-			<Typography variant="h5" color="primary">
-				Join existing rooms or create a new one
-			</Typography>
+			<Grow in timeout={1000}>
+				<Typography variant="h4" color="primary" gutterBottom>
+					Hi {props.username}
+				</Typography>
+			</Grow>
+			<Grow in timeout={1500}>
+				<Typography variant="h5" color="primary">
+					Join existing rooms or create a new one
+				</Typography>
+			</Grow>
 		</Grid>
 		<Grid item xs={12}>
-			<Typography variant="button" color="secondary">
-				By creating a new room you are a room Master, others who join
-				are spectators and can see everything you do.
-			</Typography>
+			<Grow in timeout={1500}>
+				<Typography variant="button" color="secondary">
+					By creating a new room you are a room Master, others who
+					join are spectators and can see everything you do.
+				</Typography>
+			</Grow>
 		</Grid>
 		<Grid item xs={12}>
-			<Button
-				color="primary"
-				size="large"
-				variant="contained"
-				onClick={props.handleNewRoomOpen}
-			>
-				Create new room
-			</Button>
+			<Grow in timeout={1500}>
+				<Button
+					color="primary"
+					size="large"
+					variant="contained"
+					onClick={props.handleNewRoomOpen}
+				>
+					Create new room
+				</Button>
+			</Grow>
 		</Grid>
 	</Grid>
 );
--- src/hoc/with-modal/withModal.js
+++ src/hoc/with-modal/withModal.js
@@ -1,36 +1,44 @@
 import React from "react";
 import Modal from "@material-ui/core/Modal";
+import Zoom from "@material-ui/core/Zoom";
 import PropTypes from "prop-types";
 
 import { styles } from "./stylesheet";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const withModal = WrappedComponent => {
-    class WithModal extends React.Component {
-        render() {
-            const { classes } = this.props;
+	class WithModal extends React.Component {
+		render() {
+			const { classes } = this.props;
 
-            return (
-                <Modal
-                    aria-labelledby="modal-title"
-                    aria-describedby="modal-description"
-                    className={classes.modal}
-                    open={this.props.open}
-                >
-                    <div className={classes.container}>
-                        <WrappedComponent {...this.props} classes={null} />
-                    </div>
-                </Modal>
-            );
-        }
-    }
+			return (
+				this.props.open && (
+					<Modal
+						aria-labelledby="modal-title"
+						aria-describedby="modal-description"
+						className={classes.modal}
+						open={this.props.open}
+					>
+						<Zoom in>
+							<div className={classes.container}>
+								<WrappedComponent
+									{...this.props}
+									classes={null}
+								/>
+							</div>
+						</Zoom>
+					</Modal>
+				)
+			);
+		}
+	}
 
-    WithModal.propTypes = {
-        classes: PropTypes.object.isRequired,
-        open: PropTypes.bool.isRequired
-    };
+	WithModal.propTypes = {
+		classes: PropTypes.object.isRequired,
+		open: PropTypes.bool.isRequired
+	};
 
-    return withStyles(styles)(WithModal);
+	return withStyles(styles)(WithModal);
 };
 
 export default withModal;
