1ebe8880f182699f21014e271b4b1a57708a2269
Dusan Jovanovic <dusandjovanovic@outlook.com>
2019-08-23 09:56:32 +0200

    [refactor] graph data module

--- src/utils/graph-module/graphAdapter.js
+++ src/utils/graph-module/graphAdapter.js
@@ -1,52 +1,52 @@
 export const graphAdapterNode = (node, graph) => {
-    graph.nodes.push({
-        key: node,
-        inEdges: [],
-        outEdges: []
-    });
+	graph.nodes.push({
+		key: node,
+		inEdges: [],
+		outEdges: []
+	});
 };
 
 export const graphAdapterEdge = (source, target, graph) => {
-    let srcNode = graph.nodes.findIndex(element => element.key === source);
-    let dstNode = graph.nodes.findIndex(element => element.key === target);
-    if (graph.nodes[dstNode]) {
-        graph.edges.push({
-            key: source + "->" + target,
-            source: graph.nodes[srcNode],
-            target: graph.nodes[dstNode]
-        });
-        graph.nodes.map(node => {
-            if (node.key === source) {
-                node.outEdges.push(target);
-                node.outEdges.sort((a, b) => a - b);
-            } else if (node.key === target) {
-                node.inEdges.push(source);
-                node.inEdges.sort((a, b) => a - b);
-            }
-            return true;
-        });
-    }
+	let srcNode = graph.nodes.findIndex(element => element.key === source);
+	let dstNode = graph.nodes.findIndex(element => element.key === target);
+	if (graph.nodes[dstNode]) {
+		graph.edges.push({
+			key: source + "->" + target,
+			source: graph.nodes[srcNode],
+			target: graph.nodes[dstNode]
+		});
+		graph.nodes.map(node => {
+			if (node.key === source) {
+				node.outEdges.push(target);
+				node.outEdges.sort((a, b) => a - b);
+			} else if (node.key === target) {
+				node.inEdges.push(source);
+				node.inEdges.sort((a, b) => a - b);
+			}
+			return true;
+		});
+	}
 };
 
 export const graphAdapterNodeRemove = (node, graph) => {
-    graph.edges.map(edge => {
-        if (edge.target.key === node || edge.source.key === node)
-            graphAdapterEdgeRemove(edge.source.key, edge.target.key, graph);
-        return edge;
-    });
-    graph.nodes = graph.nodes.filter(element => element.key !== node);
+	graph.edges.map(edge => {
+		if (edge.target.key === node || edge.source.key === node)
+			graphAdapterEdgeRemove(edge.source.key, edge.target.key, graph);
+		return edge;
+	});
+	graph.nodes = graph.nodes.filter(element => element.key !== node);
 };
 
 export const graphAdapterEdgeRemove = (source, target, graph) => {
-    graph.edges = graph.edges.filter(
-        element =>
-            element.source.key !== source || element.target.key !== target
-    );
-    graph.nodes.map(node => {
-        if (node.key === target)
-            node.inEdges = node.inEdges.filter(element => element !== source);
-        else if (node.key === source)
-            node.outEdges = node.outEdges.filter(element => element !== target);
-        return node;
-    });
+	graph.edges = graph.edges.filter(
+		element =>
+			element.source.key !== source || element.target.key !== target
+	);
+	graph.nodes.map(node => {
+		if (node.key === target)
+			node.inEdges = node.inEdges.filter(element => element !== source);
+		else if (node.key === source)
+			node.outEdges = node.outEdges.filter(element => element !== target);
+		return node;
+	});
 };
--- src/utils/graph-module/stackModule.js
+++ src/utils/graph-module/stackModule.js
@@ -1,17 +1,17 @@
 export const stack = (data = [], t = 0) => {
-    let dataStore = data;
-    let top = t;
+	let dataStore = data;
+	let top = t;
 
-    const stackProto = {
-        push(element) {
-            dataStore[top++] = element;
-        },
-        pop: () => dataStore.splice(--top, 1),
-        peek: () => dataStore[top - 1],
-        length: () => top,
-        isEmpty: () => top === 0,
-        getStack: () => dataStore.map(element => element)
-    };
+	const stackProto = {
+		push(element) {
+			dataStore[top++] = element;
+		},
+		pop: () => dataStore.splice(--top, 1),
+		peek: () => dataStore[top - 1],
+		length: () => top,
+		isEmpty: () => top === 0,
+		getStack: () => dataStore.map(element => element)
+	};
 
-    return Object.create(stackProto);
+	return Object.create(stackProto);
 };
